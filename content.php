0520b29decb90aa456aa93fb20ce00bf6f8b8eb9
M. Hassan Khurshid <mkshid@gmail.com>
2015-10-20 16:13:38 +0200

    Use get_template_part to make the code DRY

---
+++ content.php
@@ -0,0 +1,60 @@
+<article class="post <?php if (has_post_thumbnail()) {?>
+  has-thumbnail
+  <?php } ?> ">
+
+  <!-- post-thumbnail -->
+  <div class="post-thumbnail">
+    <a href="<?php the_permalink(); ?>">
+      <?php the_post_thumbnail('small-thumbnail'); ?>
+    </a>
+  </div>
+  <!-- post-thumbnail -->
+
+ <a href="<?php the_permalink(); ?>"> <h2><?php the_title(); ?></h2></a>
+ <p class="post-info"><?php the_time('F jS, Y g:i a'); ?> |
+ by
+ <a href="<?php get_author_posts_url(get_the_author_meta('ID')); ?>">
+  <?php the_author(); ?>
+ </a> |
+ Posted in
+
+  <?php
+    $categories = get_the_category();
+    $separtor = ", ";
+    $output = '';
+
+    if ($categories){
+      foreach ($categories as $category) {
+        $output .= '<a href="'. get_category_link($category -> term_id) .'">'. $category->cat_name .'</a>' . $separtor;
+      }
+
+      echo trim($output, $separtor);
+    }
+
+   ?>
+
+ </p>
+
+  <?php if (is_search() OR is_archive()) {
+    ?>
+    <p>
+       <?php echo get_the_excerpt(); ?>
+       <a href="<?php the_permalink(); ?>">Read more&raquo</a>
+      </p>
+  <?php
+
+    } else {
+      if ($post->post_excerpt) {
+        ?>
+      <p>
+       <?php echo get_the_excerpt(); ?>
+       <a href="<?php the_permalink(); ?>">Read more&raquo</a>
+      </p>
+      <?php
+      } else {
+       the_content();
+    }
+
+    }?>
+
+</article>
