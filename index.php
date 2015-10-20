7e3f3b15a7a7a725dccca693d98e2d0618a9ee6f
M. Hassan Khurshid <mkshid@gmail.com>
2015-10-20 17:17:55 +0200

    Add the aside post format

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ get_header();
   if (have_posts()):
     while (have_posts()): the_post();
 
-      get_template_part('content');
+      get_template_part('content', get_post_format());
 
     endwhile;
 
