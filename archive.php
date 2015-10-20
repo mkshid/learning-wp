52cd498e743ff7d56938b80149c9cf712cbafb53
M. Hassan Khurshid <mkshid@gmail.com>
2015-10-20 17:44:52 +0200

    Add get_post_format to archive and search template

--- archive.php
+++ archive.php
@@ -35,7 +35,7 @@ get_header();
 
     <?php
     while (have_posts()): the_post();
-      get_template_part('content');
+      get_template_part('content', get_post_format());
     endwhile;
 
   else:
