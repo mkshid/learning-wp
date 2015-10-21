d46aab26b2dce8c9757f487b9c4986bac4a42812
M. Hassan Khurshid <mkshid@gmail.com>
2015-10-21 09:57:00 +0200

    Add sidebar and a copied the style, really bad commit...

--- footer.php
+++ footer.php
@@ -1,4 +1,35 @@
   <footer class='site-footer'>
+
+    <!-- footer-widegts  -->
+    <div class="footer-widegts clearfix">
+
+      <?php if (is_active_sidebar('footer1')): ?>
+        <div class="footer-widget-area">
+          <?php dynamic_sidebar('footer1'); ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if (is_active_sidebar('footer2')): ?>
+        <div class="footer-widget-area">
+          <?php dynamic_sidebar('footer2'); ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if (is_active_sidebar('footer3')): ?>
+        <div class="footer-widget-area">
+          <?php dynamic_sidebar('footer3'); ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if (is_active_sidebar('footer4')): ?>
+        <div class="footer-widget-area">
+          <?php dynamic_sidebar('footer4'); ?>
+        </div>
+      <?php endif; ?>
+
+    </div>
+    <!-- /footer-widegts -->
+
     <nav class="site-nav">
       <?php
         $args = array(
--- index.php
+++ index.php
@@ -1,19 +1,31 @@
 <?php
 
-get_header();
+get_header(); ?>
+  <!-- site-content -->
+  <div class="site-content clearfix">
+    <!-- main-column -->
+    <div class="main-column">
 
-  if (have_posts()):
-    while (have_posts()): the_post();
+        <?php
+          if (have_posts()):
+            while (have_posts()): the_post();
 
-      get_template_part('content', get_post_format());
+              get_template_part('content', get_post_format());
 
-    endwhile;
+            endwhile;
 
-  else:
-    echo '<p>No content found</p>';
+          else:
+            echo '<p>No content found</p>';
 
-  endif;
+          endif; ?>
+    </div>
+    <!-- /main-column -->
+    <?php get_sidebar(); ?>
+  </div>
+  <!-- /site-content -->
 
+<?php
 get_footer();
 
 ?>
+
